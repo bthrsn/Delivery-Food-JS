e42edee0c01ef9cae5d5add8c89d867004bd6813
bthrsn <62832888+bthrsn@users.noreply.github.com>
2020-05-06 13:07:51 +0300

    Add files via upload

---
+++ delivery-food/js/main.js
@@ -0,0 +1,77 @@
+const cartButton = document.querySelector("#cart-button");
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".close");
+
+cartButton.addEventListener("click", toggleModal);
+close.addEventListener("click", toggleModal);
+
+function toggleModal() {
+  modal.classList.toggle("is-open");
+}
+
+// day 1
+
+const buttonAuth = document.querySelector(".button-auth");
+const modalAuth = document.querySelector(".modal-auth");
+const closeAuth = document.querySelector(".close-auth");
+const logInForm = document.querySelector("#logInForm");
+const loginInput = document.querySelector("#login");
+const userName = document.querySelector(".user-name");
+const buttonOut = document.querySelector(".button-out");
+let login = localStorage.getItem("gloDelivery");
+
+function toggleModalAuth() {
+  modalAuth.classList.toggle("is-open");
+}
+
+function authorized() {
+  function logOut() {
+    login = "";
+    localStorage.removeItem("gloDelivery");
+    buttonAuth.style.display = "";
+    userName.style.display = "";
+    buttonOut.style.display = "";
+    buttonOut.removeEventListener("click", logOut);
+    checkAuth();
+  }
+  console.log("Авторизован");
+  userName.textContent = login;
+  buttonAuth.style.display = "none";
+  userName.style.display = "inline";
+  buttonOut.style.display = "block";
+  buttonOut.addEventListener("click", logOut);
+}
+
+function notAuthorized() {
+  console.log("Не авторизован");
+
+  function logIn(event) {
+    event.preventDefault();
+    login = loginInput.value;
+    if (login == "") {
+      toggleModalAuth();
+      alert("Пожалуйста, введите логин!");
+    }
+    localStorage.setItem("gloDelivery", login);
+    toggleModalAuth();
+    buttonAuth.removeEventListener("click", toggleModalAuth);
+    closeAuth.removeEventListener("click", toggleModalAuth);
+    logInForm.removeEventListener("submit", logIn);
+    logInForm.reset();
+    checkAuth();
+  }
+
+  buttonAuth.addEventListener("click", toggleModalAuth);
+  closeAuth.addEventListener("click", toggleModalAuth);
+  logInForm.addEventListener("submit", logIn);
+}
+
+function checkAuth() {
+  if (login) {
+    authorized();
+  } else {
+    notAuthorized();
+  }
+}
+
+checkAuth();
